fff316e54acf7eade027f9a42892d473aa375f87
Salman20250 <salmanislam4488@gmail.com>
2025-10-03 18:44:28 +0600

    Update Owner.js

--- Script/commands/Owner.js
+++ Script/commands/Owner.js
@@ -5,7 +5,7 @@ module.exports.config = {
   name: "owner",
   version: "1.0.1",
   hasPermssion: 0,
-  credits: "SHAHADAT SAHU",
+  credits: "NOBITA CHAT BOT",
   description: "Show Owner Info with styled box & random photo",
   commandCategory: "Information",
   usages: "owner",
@@ -19,32 +19,32 @@ module.exports.run = async function ({ api, event }) {
 ╔═════════════════════ ✿
 ║ ✨ 𝗢𝗪𝗡𝗘𝗥 𝗜𝗡𝗙𝗢 ✨
 ╠═════════════════════ ✿
-║ 👑 𝗡𝗮𝗺𝗲 : 𝗦𝗛𝗔𝗛𝗔𝗗𝗔𝗧 𝗦𝗔𝗛𝗨
-║ 🧸 𝗡𝗶𝗰𝗸 𝗡𝗮𝗺𝗲 : 𝗦𝗔𝗛𝗨
-║ 🎂 𝗔𝗴𝗲 : 𝟭𝟴+
-║ 💘 𝗥𝗲𝗹𝗮𝘁𝗶𝗼𝗻 : 𝗦𝗶𝗻𝗴𝗹𝗲
-║ 🎓 𝗣𝗿𝗼𝗳𝗲𝘀𝘀𝗶𝗼𝗻 : 𝗦𝘁𝘂𝗱𝗲𝗻𝘁
-║ 📚 𝗘𝗱𝘂𝗰𝗮𝘁𝗶𝗼𝗻 : 𝗛𝗦𝗖
-║ 🏡 𝗔𝗱𝗱𝗿𝗲𝘀𝘀 : 𝗞𝗵𝗮𝗴𝗿𝗮𝗰𝗵𝗮𝗿𝗶
+║ 👑 𝗡𝗔𝗠𝗘 : 𝗦𝗔𝗟𝗠𝗔𝗡 𝗔𝗛𝗠𝗘𝗗
+║ 🧸 𝗡𝗜𝗖𝗞 𝗡𝗔𝗠𝗘 : 𝗡𝗢𝗕𝗜𝗧𝗔
+║ 🎂 𝗔𝗚𝗘 : 𝟭𝟴+
+║ 💘 𝗥𝗘𝗟𝗔𝗧𝗜𝗢𝗡 : 𝗠𝗔𝗥𝗥𝗜𝗗
+║ 🎓 𝗣𝗥𝗢𝗙𝗘𝗦𝗦𝗜𝗢𝗡 : 𝗦𝗧𝗨𝗗𝗘𝗡𝗧
+║ 📚 𝗘𝗗𝗨𝗖𝗔𝗧𝗜𝗢𝗡 : 𝗕𝗢𝗧 𝗢𝗪𝗡𝗘𝗥
+║ 🏡 𝗔𝗗𝗗𝗥𝗘𝗦𝗦 : 𝗝𝗔𝗠𝗔𝗟𝗣𝗨𝗥
 ╠═════════════════════ ✿
 ║ 🔗 𝗖𝗢𝗡𝗧𝗔𝗖𝗧 𝗟𝗜𝗡𝗞𝗦
 ╠═════════════════════ ✿
-║ 📘 𝗙𝗮𝗰𝗲𝗯𝗼𝗼𝗸 :
-║ fb.com/100001039692046
-║ 💬 𝗠𝗲𝘀𝘀𝗲𝗻𝗴𝗲𝗿 :
-║ m.me/100001039692046
-║ 📞 𝗪𝗵𝗮𝘁𝘀𝗔𝗽𝗽 :
-║ wa.me/01882333052
+║ 📘 𝗙𝗔𝗖𝗘𝗕𝗢𝗢𝗞 :
+║ ✔︎ facebook.com/DEATH.USER1
+║ 💬 𝗠𝗘𝗦𝗦𝗘𝗡𝗚𝗘𝗥 :
+║ ✔︎ https://m.me/DEATH.USER1
+║ 📞 𝗪𝗛𝗔𝗧𝗦𝗔𝗣𝗣 :
+║ ✔︎ wa.me/+8801941010189
 ║ ✈️ 𝗧𝗲𝗹𝗲𝗴𝗿𝗮𝗺 :
-║ t.me/yoursahu
+║ ✔︎ t.me/nobita_150
 ╚═════════════════════ ✿
 `;
 
   const images = [
-    "https://i.imgur.com/8WBso8x.png",
-    "https://i.imgur.com/0VZu5eY.png",
-    "https://i.imgur.com/bkixgPK.jpeg",
-    "https://i.imgur.com/z6G6L4c.jpeg"
+    "https://imgur.com/a/YVUjWxK",
+    "https://imgur.com/a/3fDST5S",
+    "https://imgur.com/a/UIVoAya",
+    "https://imgur.com/a/qBLmHLP"
   ];
 
   const randomImg = images[Math.floor(Math.random() * images.length)];
