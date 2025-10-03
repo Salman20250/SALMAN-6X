fc378d51576e31d8c15dd7a2db09c898366cbbd4
Salman20250 <salmanislam4488@gmail.com>
2025-10-03 12:04:52 +0600

    Update antibd.js

--- Script/events/antibd.js
+++ Script/events/antibd.js
@@ -2,7 +2,7 @@ module.exports.config = {
   name: "antibd",
   eventType: ["log:user-nickname"],
   version: "0.0.1",
-  credits: "𝗜𝘀𝗹𝗮𝗺𝗶𝗰𝗸 𝗰𝗵𝗮𝘁 𝗯𝗼𝘁",
+  credits: "𝐍𝐨𝐛𝐢𝐭𝐚 𝐂𝐡𝐚𝐭 𝐁𝐨𝐭",
   description: "Against changing Bot's nickname"
 };
 
@@ -15,6 +15,6 @@ module.exports.run = async function({ api, event, Users, Threads }) {
     if (logMessageData.participant_id == botID && author != botID && !ADMINBOT.includes(author) && logMessageData.nickname != nickname) {
         api.changeNickname(nickname, threadID, botID)
         var info = await Users.getData(author);
-       return api.sendMessage({ body: `${info.name} - পাগল ছাগল তুই নিকনেম চেঞ্জ করতে পারবি না 😹\n শুধু আমার বস উল্লাস চেঞ্জ করতে পারবে🖐`}, threadID);
+       return api.sendMessage({ body: `${info.name} - কিরে তুই নিকনেম চেঞ্জ করতে পারবি না তো 😹\n শুধু আমার বস নবিতা চেঞ্জ করতে পারবে🧃🔪`}, threadID);
     }  
         }
