ac587bd7f345bd66f61075781066d2e405f29622
Salman20250 <salmanislam4488@gmail.com>
2025-10-04 08:47:39 +0600

    Update Admin.js

--- Script/commands/Admin.js
+++ Script/commands/Admin.js
@@ -49,7 +49,7 @@ module.exports.run = async function({ api, event }) {
  attachment: fs.createReadStream(__dirname + "/cache/owner.jpg")
  }, event.threadID, () => fs.unlinkSync(__dirname + "/cache/owner.jpg"));
 
- return request("https://imgur.com/a/TWPDxHY")
+ return request("https://i.imgur.com/WdQrEsX.jpeg")
  .pipe(fs.createWriteStream(__dirname + '/cache/owner.jpg'))
  .on('close', () => callback());
 };
