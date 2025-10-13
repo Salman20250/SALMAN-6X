d35b1006deb7f41a70520a0a9928fdd92ed6e439
nobita5300 <salmanislam4488@gmail.com>
2025-10-13 10:51:45 +0600

    Update x.js

--- Script/commands/x.js
+++ Script/commands/x.js
@@ -2,7 +2,7 @@ module.exports.config = {
 	name: "x",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "NOBITA CHAT BOT",
 	description: "Make the bot return google's audio file via text",
 	commandCategory: "media",
 	usages: "[ru/en/ko/ja/tl] [Text]",
