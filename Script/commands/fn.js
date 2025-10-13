e7d1e46142ca70b2177ff2e108a78dfbda7c5043
nobita5300 <salmanislam4488@gmail.com>
2025-10-13 12:36:39 +0600

    Update and rename chor.js to fn.js

--- Script/commands/fn.js
+++ Script/commands/fn.js
@@ -1,8 +1,8 @@
 module.exports.config = {
-	name: "chor",
+	name: "fn",
 	version: "1.0.1",
 	hasPermssion: 0,
-	credits: "𝐂𝐘𝐁𝐄𝐑 ☢️_𖣘 -𝐁𝐎𝐓 ⚠️ 𝑻𝑬𝑨𝑴_ ☢️",
+	credits: "NOBITA CHAT BOT",
  description: "scooby doo template memes",
 	commandCategory: "Picture",
 	usages: "...",
@@ -41,7 +41,7 @@ try {
 	ctx.drawImage(await Canvas.loadImage(avatar), 48, 410, 111, 111);
 	const imageBuffer = canvas.toBuffer();
 	fs.writeFileSync(path_toilet,imageBuffer);
-	 api.sendMessage({attachment: fs.createReadStream(path_toilet, {'highWaterMark': 128 * 1024}), body: "বলদ মেয়েদের চিপায় ধরা খাইছে😁😁"}, event.threadID, () => fs.unlinkSync(path_toilet), event.messageID);
+	 api.sendMessage({attachment: fs.createReadStream(path_toilet, {'highWaterMark': 128 * 1024}), body: "✔︎ দেখো সবাই বলদ মেয়েদের চিপায় ধরা খাইছে😂😁"}, event.threadID, () => fs.unlinkSync(path_toilet), event.messageID);
 }
 catch(e) {api.sendMessage(e.stack, event.threadID )}
  }
